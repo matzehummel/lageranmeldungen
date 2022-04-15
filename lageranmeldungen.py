68c950a973d5419eb587d3f59a61c0608375b1ad
ma-hu <ma-hummel@gmx.de>
2022-04-15 16:29:14 +0200

    print outs

--- lageranmeldungen.py
+++ lageranmeldungen.py
@@ -1,32 +1,33 @@
 from os import walk
 
+
+print("Lageranmeldungen-Parser started...")
 path = "C:/Projects/Automation/Lageranmeldungen/exports"
+print("Path: " + path)
 
 files = []
 for (dirpath, dirnames, filenames) in walk(path):
     files.extend(filenames)
     break
+print(str(len(files)) + " Documents found.")
 
 signUps = open("Lageranmeldungen.csv", "a")
 
 
-
 def parseFile(filename):
+    print("Parsing " + filename + " ...")
     file = open(path + "/" + filename)
     content = file.read()
     content = content.split("Nachname")
-    print(content)
     signUpEntries = content[1].split("<br/>")
     if(signUpEntries[-1].strip() == ""):
         signUpEntries.pop(-1)
 
     counter = 0
     surname = (signUpEntries[counter].split(":"))[1].strip()
-    print(surname)
 
     counter += 1
     name = signUpEntries[counter].split(":")[1].strip()
-    print(name)
 
     counter += 1    # 1
     type = (signUpEntries[counter].split(":"))[1].strip()
@@ -34,16 +35,13 @@ def parseFile(filename):
         type = "XXL"
     else:
         type = "Kinderlager"
-    print(type)
 
     counter += 1    # 2
     birthdate = (signUpEntries[counter].split(":"))[1].strip()
     birthdate = birthdate.replace(" / ", ".")
-    print(birthdate)
 
     counter += 1    # 3
     email = (signUpEntries[counter].split(":"))[1].strip()
-    print(email)
 
     counter += 1    # 4
     phoneEntry = signUpEntries[counter].split(":")
@@ -51,10 +49,8 @@ def parseFile(filename):
     if(phoneEntry[0].strip() == "Telefonnummer"):
         phone = phoneEntry[1].strip()
         counter += 1    # 5
-    print(phone)
 
     address = signUpEntries[counter].split(":")[1].strip()
-    print(address)
 
     counter += 1    # 6 or 7
     postalCode = signUpEntries[counter].split(":")[1].strip()
@@ -64,7 +60,6 @@ def parseFile(filename):
 
     counter += 1    # 8 or 9
     memberOf = signUpEntries[counter].split(":")[1].strip()
-    print(memberOf)
 
     counter += 1    # 9 or 10
     experience = signUpEntries[counter].split(":")[1].strip()
@@ -72,20 +67,19 @@ def parseFile(filename):
         experience = "ja"
     else:
         experience = "nein"
-    print(experience)
 
     counter += 1    # 10 or 11
     supervisors = "no supervisors"
     if(len(signUpEntries) > counter ):
-        print(len(signUpEntries))
-        print(counter)
         supervisors = signUpEntries[counter].split(":")[2].strip()
-    print(supervisors)
 
     return surname + "\t" + name  + "\t" + type + "\t" + birthdate + "\t" + email + "\t" + phone + "\t" + address + "\t" + postalCode + "\t" + city + "\t" + memberOf + "\t" + experience + "\t" + supervisors
 
 for file in files:
     newline = parseFile(file)
     signUps.write(newline + "\n")
+
+
+print("Done")
 
 signUps.close()
